c3017c81874acd7915e13a70aa540f0bb01ce141
İlker Bugüner <ilker.buguner@gmail.com>
2019-10-28 19:26:14 +0200

    Made simple mini ORM.

---
+++ 02-Entity Framework Core/02-Workshop MiniORM/MiniORM Core/MiniORM.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    public class Program
+    {
+        private static string connectionString = @"Server=.\SQLEXPRESS;Database=MiniORM;Integrated Security=True;";
+        static void Main(string[] args)
+        {
+            var db = new SoftUniDbContext(connectionString);
+
+            db.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = db.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = db.Employees.Last();
+            employee.FirstName = "Modified";
+
+            db.SaveChanges();
+        }
+    }
+}
